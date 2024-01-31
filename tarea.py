4876157d6fa573c3611cc8f8b22f3c248e44200b
ChefanRFG <158303850+ChefanRFG@users.noreply.github.com>
2024-01-31 09:19:16 -0600

    Version 1

---
+++ tarea.py
@@ -0,0 +1,13 @@
+partido_rojo=""
+eleccion=""
+dpi=0
+partido=""
+partido_azul=""
+rojo=""
+azul=""
+dpi=(input("ingrese su dpi:"))
+print("partidos: ")
+print("1. Partido_azul")
+print("2. partido:rojo")
+eleccion=input("indique a que partido es su voto:")
+if eleccion = "rojo"
